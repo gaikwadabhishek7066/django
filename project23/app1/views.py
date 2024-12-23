db257502cfdfd6dac7308c195789b68a12859f19
gaikwadabhishek7066 <gaikwadabhishek91102@gmail.com>
2024-12-22 22:48:48 -0800

    push commit all

--- project23/app1/views.py
+++ project23/app1/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
 # Create your views here.
+def cart_view(request):
+    b1 = request.GET['b1']
+    if b1 =="AddtoCart" :
+        pname = request.GET['pname']
+        qty = request.GET['qty']
+        response = render(request, "app1/index.html",context={'msg':"Project to add cart"})
+        response.set_cookie(pname,qty)
+        return response
+    elif b1 == "UpdatetoCart":
+        pname = request.GET['pname']
+        qty = request.GET['qty']
+        response = render(request, "app1/index.html", context={'msg':"project to update cart"})
+        response.set_cookie(pname,qty)
+        return response
+    elif b1 == "DeletefromCart":
+        pname = request.GET['pname']
+        qty = request.GET['qty']
+        response = render(request, "app1/index.html", context={'msg':"project to delete cart"})
+        response.delete_cookie(pname)
+        return response
+    elif b1 == "ViewCart":
+        output = ''
+        for pname,qty in request.COOKIES.items():
+            output=output+pname+":"+qty+"<br>"''    
+        response = render(request, "app1/index.html", context={'msg':output})
+        return response
+    
+def index(request):
+    response = render(request, "app1/index.html")
+    return response
+
--- project23/project23/urls.py
+++ project23/project23/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index),
+    path('cart/',cart_view)
 ]
