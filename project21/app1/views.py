c73d6326a6200bec15d60851d0713b33e685d5a5
gaikwadabhishek7066 <gaikwadabhishek91102@gmail.com>
2024-12-22 22:46:44 -0800

    push commit all

--- project21/app1/views.py
+++ project21/app1/views.py
@@ -3,10 +3,9 @@ import datetime
 # Create your views here.
 
 def show_employee_data(request):
-    emp_data={101:['naresh',datetime.date(2020,8,12)],
-              102:['suresh',datetime.date(2020,9,10)],
-              103:['ramesh',datetime.date(2020,10,11)],
-              104:['suresh',datetime.date(2022,10,14)]
-              }
+    emp_data={101:['naresh',5000],
+              102:['suresh',6000],
+              103:['',9000],
+              104:['ramesh',0]}
     response=render(request,'app1/show_emp.html',{'emp_data':emp_data})
     return response
