72f1b9b53c1b4c245501c6c53c30618adea925ff
gaikwadabhishek7066 <gaikwadabhishek91102@gmail.com>
2025-01-06 13:53:40 -0800

    push all commm

---
+++ project_4_1/app1/resttest.py
@@ -0,0 +1,25 @@
+import requests
+
+BASE_URL="http://localhost:8000/"
+def list_employees():
+    END_POINT="listemp"
+    response=requests.get(BASE_URL+END_POINT)
+    json_data=response.json()
+    print(json_data)
+
+def read_employee(empno):
+    END_POINT="reademp/"+str(empno)
+    response=requests.get(BASE_URL+END_POINT)
+    json_data=response.json()
+    print(json_data)
+
+print("1.List Employee")
+print("2.Display Employee")
+opt=int(input("Enter your Option"))
+if opt==1:
+    list_employees()
+elif opt==2:
+    empno=int(input("EmployeeNo:"))
+    read_employee(empno)
+    
+    
---
+++ project_4_1/app1/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.core.serializers import serialize
+from app1.models import Employee
+from django.http import JsonResponse
+import json
+# Create your views here.
+
+def list_employees(request):
+    qs = Employee.objects.all()
+    json_data = serialize("json", qs)
+    dict1=json.loads(json_data)
+    data=[]
+    for row in dict1:
+        row['fields']['empno']=row['pk']
+        data.append(row['fields'])
+    json_data=json.dumps(data)
+    response=JsonResponse(json_data, safe=False)
+    return response
+
+def read_employee(request, empno):
+    emp=Employee.objects.get(empno=empno)
+    json_data=serialize("json", [emp])
+    response = JsonResponse(json_data, safe=False)
+    return response
