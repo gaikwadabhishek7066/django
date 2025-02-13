e776c88883501f98427a510979eb299fc1fd1a33
gaikwadabhishek7066 <gaikwadabhishek91102@gmail.com>
2025-02-13 01:09:04 -0800

    push all commm

---
+++ ecommerce/cart/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(null=True)
+    image = models.ImageField(upload_to='product/')
+
+    def __str__(self):
+        return self.name
+    
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name}"
